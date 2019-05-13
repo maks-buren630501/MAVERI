d009ab6598888d6587272dcbe831b95abce4e43b
KashpirRoman630501 <37397486+KomaRash@users.noreply.github.com>
2019-05-13 11:03:27 +0300

    add RequestList FragmentList

--- app/src/main/java/com/example/wakeparkby/pchell/maveri/Group/GroupListSrc/GroupListFragment.java
+++ app/src/main/java/com/example/wakeparkby/pchell/maveri/Group/GroupListSrc/GroupListFragment.java
@@ -2,6 +2,8 @@ package com.example.wakeparkby.pchell.maveri.Group.GroupListSrc;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ListView;
 
 import com.example.wakeparkby.pchell.maveri.Group.Group;
 import com.example.wakeparkby.pchell.maveri.Profile.Profile;
@@ -19,5 +21,11 @@ public class GroupListFragment extends ListFragment {
         groups =profile.getGroups().getGroups();
         arrayAdapter=new AdapterGroupArray(getActivity(), groups);
         setListAdapter(arrayAdapter);
+    }
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        //получение нажатого значения листа в списке груп
+        groups.get(position);
     }
 }
